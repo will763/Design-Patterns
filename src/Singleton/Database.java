c79dc24884304570c5845dce0faa39de99779af1
will763 <killzasper@gmail.com>
2023-01-02 14:32:22 -0300

    feat: created private constructor to make the connection with the database.

--- src/Singleton/Database.java
+++ src/Singleton/Database.java
@@ -1,7 +1,25 @@
 package Singleton;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Database {
     private static Connection instance = null;
+
+    private Database(){
+
+        String driverName = "com.mysql.jdbc.Driver";
+
+        try {
+            Class.forName(driverName);
+
+            instance= DriverManager.getConnection(
+                     "jdbc:mysql://localhost:3306/user","root","123456");
+
+            System.out.println("Status ---> connection created");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
