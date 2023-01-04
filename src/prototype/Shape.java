670f1fdb7e0d1cd3750760bc7caf30f876d4d2ba
will763 <killzasper@gmail.com>
2023-01-03 23:54:32 -0300

    refactor: override the equals method.

--- src/prototype/Shape.java
+++ src/prototype/Shape.java
@@ -1,5 +1,7 @@
 package prototype;
 
+import java.util.Objects;
+
 public abstract class Shape {
     public int width;
     public int heigth;
@@ -17,4 +19,12 @@ public abstract class Shape {
     }
 
     public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Shape)) return false;
+        Shape shape2 = (Shape) object2;
+        return shape2.width == width && shape2.heigth == heigth && Objects.equals(shape2.color, color);
+    }
+
 }
