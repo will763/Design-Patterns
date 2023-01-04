73fe9cf57534942c6245fdc8d56580098a1c9e65
will763 <killzasper@gmail.com>
2023-01-04 01:23:15 -0300

    style: removed all comments.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Test for Singleton Pattern
-        /* Connection foo = Database.getInstance();
-           Connection bar = Database.getInstance();
-           System.out.println(foo.equals(bar)); */
-
     }
 }
