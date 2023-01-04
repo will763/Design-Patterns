62d67e500b48ad3de7173f131f80ca42eb520814
will763 <killzasper@gmail.com>
2023-01-04 00:46:46 -0300

    refactor: demo has been updated to use prototype factory.

--- src/prototype/Demo.java
+++ src/prototype/Demo.java
@@ -1,47 +1,28 @@
 package prototype;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Demo {
     public static void main(String[] args) {
-        List<Shape> shapes = new ArrayList<>();
-        List<Shape> shapesCopy = new ArrayList<>();
+            BundledShapeCache cache = new BundledShapeCache();
 
-        Circle circle = new Circle();
-        circle.width = 10;
-        circle.heigth = 20;
-        circle.radius = 15;
-        circle.color = "red";
-        shapes.add(circle);
+            Shape shape1 = cache.get("Big green circle");
+            Shape shape2 = cache.get("Medium blue rectangle");
+            Shape shape3 = cache.get("Medium blue rectangle");
 
-        Circle anotherCircle = (Circle) circle.clone();
-        shapes.add(anotherCircle);
+            if (shape1 != shape2 && !shape1.equals(shape2)) {
+                System.out.println("Big green circle != Medium blue rectangle (yay!)");
+            } else {
+                System.out.println("Big green circle == Medium blue rectangle (booo!)");
+            }
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.width = 10;
-        rectangle.height = 20;
-        rectangle.color = "blue";
-        shapes.add(rectangle);
-
-        cloneAndCompare(shapes, shapesCopy);
-    }
-    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
-        for (Shape shape : shapes) {
-            shapesCopy.add(shape.clone());
-        }
-
-        for (int i = 0; i < shapes.size(); i++) {
-            if (shapes.get(i) != shapesCopy.get(i)) {
-                System.out.println(i + ": Shapes are different objects (yay!)");
-                if (shapes.get(i).equals(shapesCopy.get(i))) {
-                    System.out.println(i + ": And they are identical (yay!)");
+            if (shape2 != shape3) {
+                System.out.println("Medium blue rectangles are two different objects (yay!)");
+                if (shape2.equals(shape3)) {
+                    System.out.println("And they are identical (yay!)");
                 } else {
-                    System.out.println(i + ": But they are not identical (booo!)");
+                    System.out.println("But they are not identical (booo!)");
                 }
             } else {
-                System.out.println(i + ": Shape objects are the same (booo!)");
+                System.out.println("Rectangle objects are the same (booo!)");
             }
         }
     }
-}
