57a4e675a9aa726fc9cdebabd2e91b8ce43b2c74
will763 <killzasper@gmail.com>
2023-01-03 23:02:13 -0300

    feat: added constructors in class.

--- src/prototype/Shape.java
+++ src/prototype/Shape.java
@@ -4,4 +4,15 @@ public abstract class Shape {
     public int width;
     public int heigth;
     public String color;
+
+    public Shape() {
+    }
+
+    public Shape(Shape target) {
+        if (target != null) {
+            this.width = target.width;
+            this.heigth = target.heigth;
+            this.color = target.color;
+        }
+    }
 }
