0f240acb19b007f878f29cd92708c5201325e028
will763 <killzasper@gmail.com>
2023-01-04 00:36:54 -0300

    feat: created Prototype factory.

---
+++ src/prototype/BundledShapeCache.java
@@ -0,0 +1,33 @@
+package prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BundledShapeCache {
+    private Map<String, Shape> cache = new HashMap<>();
+
+    public BundledShapeCache() {
+        Circle circle = new Circle();
+        circle.width = 5;
+        circle.heigth = 7;
+        circle.radius = 45;
+        circle.color = "Green";
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 8;
+        rectangle.height = 10;
+        rectangle.color = "Blue";
+
+        cache.put("Big green circle", circle);
+        cache.put("Medium blue rectangle", rectangle);
+    }
+
+    public Shape put(String key, Shape shape) {
+        cache.put(key, shape);
+        return shape;
+    }
+
+    public Shape get(String key) {
+        return cache.get(key).clone();
+    }
+}
