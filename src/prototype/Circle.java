28cc36d9231519766b20c7ed11b4469c16fd01fb
will763 <killzasper@gmail.com>
2023-01-04 00:02:57 -0300

    feat: added constructors.

--- src/prototype/Circle.java
+++ src/prototype/Circle.java
@@ -3,6 +3,16 @@ package prototype;
 public class Circle extends Shape{
     public int radius;
 
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
     @Override
     public Shape clone() {
         return null;
