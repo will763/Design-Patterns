e1b9def5f855435fbd1756a8693acfa353c7369b
will763 <killzasper@gmail.com>
2023-01-04 23:44:48 -0300

    feat: created HtmlButton class.

---
+++ src/factoryMethod/HtmlButton.java
@@ -0,0 +1,13 @@
+package factoryMethod;
+
+public class HtmlButton implements Button{
+    @Override
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+    @Override
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
+}
