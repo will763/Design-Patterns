c549cb782b5d830174e553cf5bdc935ae9d068b6
will763 <killzasper@gmail.com>
2023-01-05 00:05:22 -0300

    feat: created Demo class.

---
+++ src/factoryMethod/Demo.java
@@ -0,0 +1,23 @@
+package factoryMethod;
+
+public class Demo {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    static void configure() {
+        if (System.getProperty("os.name").equals("Linux")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+
+}
