27c394e21f9b9361b94e16481c2620c8c5a2bd33
will763 <killzasper@gmail.com>
2023-01-04 23:51:24 -0300

    feat: created Dialog abstract class.

---
+++ src/factoryMethod/Dialog.java
@@ -0,0 +1,10 @@
+package factoryMethod;
+
+public abstract class Dialog {
+    public void renderWindow() {
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    public abstract Button createButton();
+}
