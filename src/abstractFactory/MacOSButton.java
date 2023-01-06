5e1584c0c7d6e837fb2ba9c85101da33faba7464
will763 <killzasper@gmail.com>
2023-01-05 23:57:28 -0300

    feat: created MacOSButton that implements the Button interface.

---
+++ src/abstractFactory/MacOSButton.java
@@ -0,0 +1,8 @@
+package abstractFactory;
+
+public class MacOSButton implements Button{
+    @Override
+    public void paint() {
+        System.out.println("You have created MacOSButton.");
+    }
+}
