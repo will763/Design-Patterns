b0574265abea11ad99198ba3e6efb0d3973e8a4c
will763 <killzasper@gmail.com>
2023-01-07 23:49:08 -0300

    feat: implemented Demo class.

--- src/objectPool/Demo.java
+++ src/objectPool/Demo.java
@@ -1,4 +1,22 @@
 package objectPool;
 
 public class Demo {
+    public static void main(String[] args){
+        OliphauntPool pool = new OliphauntPool();
+        Oliphaunt oliphaunt1 = pool.checkOut();
+        Oliphaunt oliphaunt2 = pool.checkOut();
+        Oliphaunt oliphaunt3 = pool.checkOut();
+
+        System.out.println(pool);
+
+        pool.checkIn(oliphaunt1);
+        pool.checkIn(oliphaunt2);
+
+        System.out.println(pool);
+
+        Oliphaunt oliphaunt4 = pool.checkOut();
+        Oliphaunt oliphaunt5 = pool.checkOut();
+
+        System.out.println(pool);
+    }
 }
