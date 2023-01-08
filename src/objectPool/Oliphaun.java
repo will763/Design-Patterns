34b7263d8d9d8d687399d9d3ba0d8158a7f4f16e
will763 <killzasper@gmail.com>
2023-01-07 23:24:27 -0300

    feat: implemented class Oliphaun.

--- src/objectPool/Oliphaun.java
+++ src/objectPool/Oliphaun.java
@@ -1,4 +1,27 @@
 package objectPool;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Oliphaun {
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    private int id;
+
+    public void Oliphaunt() {
+        id = counter.incrementAndGet();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Oliphaunt id=%d", id);
+    }
 }
